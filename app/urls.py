d0a9dfff0d9cbe17e14f29879207ae4db42d2ebf
adadesions <adadesions@gmail.com>
2018-03-10 14:10:39 +0700

    Add Marathon page

--- app/urls.py
+++ app/urls.py
@@ -3,7 +3,8 @@ from app import views
 
 app_name = 'app'
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^register/$', views.register, name='register'),
-    url(r'^more-register/$', views.more_register, name='more_register'),
+    url('', views.index, name='index'),
+    url('register/', views.register, name='register'),
+    url('more-register/', views.more_register, name='more_register'),
+    url('marathon-exercise/', views.marathon, name='marathon'),
 ]
